8e274f087843cfaf408a2d09870a98571830f8d1
Harry Wu <dh.wuho@gmail.com>
2019-03-17 18:48:17 -0400

    temp

--- Project_2/m_tree.cc
+++ Project_2/m_tree.cc
@@ -10,7 +10,15 @@ struct m_tree_t {
     int leftmin;
     int rightmax;
     int measure;
-    m_tree_t(int k): key(k), left(NULL), right(NULL), height(1), measure(0) {}
+    m_tree_t(int k): key(k), left(NULL), right(NULL), height(1), 
+        l_value(0), l_value(0), leftmin(0), rightmax(0), measure(0) {}
+};
+
+struct interval_list {
+    int left_point;
+    int right_point;
+    interval_list* next;
+    interval_list(int a, int b): left_point(a), right_point(b), next(NULL) {}
 };
 
 int get_height(m_tree_t* node) {
@@ -33,6 +41,10 @@ int get_balance_factor(m_tree_t* node) {
 
 int max(int a, int b) {
     return a > b ? a : b;
+}
+
+int min(int a, int b) {
+    return a > b ? b : a;
 }
 
 void set_measure(m_tree_t* node)
@@ -58,6 +70,23 @@ void set_measure(m_tree_t* node)
         node->left->rightmax < node->r_value) {
         node->measure = node->right->measure + node->left->measure;
     }
+}
+
+void set_min_max(m_tree_t* node) {
+    if(node != NULL) {
+        if(node->left->leftmin < node->right->leftmin) {
+            node->leftmin = node->left->leftmin;
+        }
+        else {
+            node->leftmin = node->right->leftmin;
+        }
+        if(node->left->rightmax < node->right->rightmax) {
+            node->rightmax = node->right->rightmax;
+        }
+        else {
+            node->rightmax = node->left->rightmax;
+        }
+    } 
 }
 
 m_tree_t* left_rotate(m_tree_t* root) {
@@ -128,7 +157,7 @@ void insert_node(m_tree_t* root, int key) {
     }
     
     if(root->key > key) {
-        insert_node(root->left, key);
+        root->left = insert_node(root->left, key);
     }
     else {
         insert_node(root->right, key);
